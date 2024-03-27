db85aa4ae48a24d4477f2b8063e33c7f59e3f507
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:52:07 -0300

    Exercício 8 - Enrico Ricarte

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-//Imprimindo na tela + fundo do site
-let nome = prompt("Digite seu nome: ")
-let idade3 = prompt("Digite sua idade: ")
-let curso = prompt("Digite seu curso")
-let ano = prompt("Digite seu ano da turma: ")
-document.write(".",nome,"<br>")
-document.write(".",idade3," anos <br>")
-document.write(".",curso,"<br>")
-document.write(".",ano,"<br>")
+// Buscador de termos
+let texto = "Tecnologia é um produto da ciência e da engenharia que envolve um conjunto de instrumentos, métodos e técnicas que visam a resolução de problemas";
+console.log(texto.indexOf("em"))
+console.log(texto.lastIndexOf("ia"))
+console.log(texto.slice(27,34))
+console.log(texto.slice(92,98))
