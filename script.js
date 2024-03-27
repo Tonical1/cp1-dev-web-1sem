9f64e0e3c60e7b7fc2a4099d6a192d6a291a8ed1
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:44:49 -0300

    Exercício 4 - Enrico Ricarte

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-//Cálculo de IMC
-let peso = parseFloat(prompt("Digite seu peso em KG: "));
-let altura = parseFloat(prompt("Digite sua altura em m: "));
-let imc = parseFloat( peso/(altura**2) );
-
-if(imc < 18.5){
-    console.log(imc,":Abaixo do peso")
-} 
-else if (18.5 <= imc && imc <=24.9 ){
-    console.log(imc,":Peso ideal")
+//Catalogo idade
+let idade2 = parseInt(prompt("digite a idade a ser catalogada: "));
+if(idade2 !=0){
+    if(idade2<=12){
+        console.log(idade2,"Trata-se de uma criança!")
+    }
+    else if (idade2>=13 && idade2 <=18){
+        console.log(idade2,"Trata-se de um adolescente!")
+    }
+    else if(idade2>=19 && idade2<=60){
+        console.log(idade2,"Trata-se de um adulto!")
+    }
+    else{
+        console.log(idade2,"Trata-se de um idoso!")
+    }
 }
 else{
-    console.log(imc,":Acima do peso")
+    console.log("Insira uma idade válida! (<0)")
 }
