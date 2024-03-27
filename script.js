be0aae6710c23708268459c28561500588969b45
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:41:21 -0300

    Exercício 1 - Victor Freire

---
+++ script.js
@@ -0,0 +1,11 @@
+// Resolvendo questões da CP1 - web dev
+// Declarando variavel undefined / vazio / com valor
+
+let indefinida;
+console.log(indefinida)
+
+let vazia = null;
+console.log(vazia)
+
+let comvalor = "Com valor!";
+console.log(comvalor)
