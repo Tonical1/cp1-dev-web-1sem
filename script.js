112e5ca623b4b1d750840b93d2796e8d3f850528
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:50:44 -0300

    Exercício 7 - Eduardo Levy

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-//Cálculo de aprovação
- 
-let nota1 = parseFloat(prompt("Digite a primeira nota da média: "));
-let nota2 = parseFloat(prompt("Digite a segunda nota da média: "));
-let nota3 = parseFloat(prompt("Digite a terceira nota da média: "));
-let nota4 = parseFloat(prompt("Digite a quarta nota da média: "));
-let nota5 = parseFloat(prompt("Digite a quinta nota da média: "));
-let nota6 = parseFloat(prompt("Digite a sexta nota da média: "));
-let nota7 = parseFloat(prompt("Digite a sétima nota da média: "));
-let media = parseFloat((nota1+nota2+nota3+nota4+nota5+nota6+nota7)/7)
-if(media>=6){
-    console.log(media,"Aprovado!")
-}
-else{
-    console.log(media,"Reprovado.")
-}
+//Imprimindo na tela + fundo do site
+let nome = prompt("Digite seu nome: ")
+let idade3 = prompt("Digite sua idade: ")
+let curso = prompt("Digite seu curso")
+let ano = prompt("Digite seu ano da turma: ")
+document.write(".",nome,"<br>")
+document.write(".",idade3," anos <br>")
+document.write(".",curso,"<br>")
+document.write(".",ano,"<br>")
