2ee93afc8bc76077a487fb7f277ec4874977b253
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:47:06 -0300

    Exercício 5 - Victor Freire

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-//Catalogo idade
-let idade2 = parseInt(prompt("digite a idade a ser catalogada: "));
-if(idade2 !=0){
-    if(idade2<=12){
-        console.log(idade2,"Trata-se de uma criança!")
-    }
-    else if (idade2>=13 && idade2 <=18){
-        console.log(idade2,"Trata-se de um adolescente!")
-    }
-    else if(idade2>=19 && idade2<=60){
-        console.log(idade2,"Trata-se de um adulto!")
-    }
-    else{
-        console.log(idade2,"Trata-se de um idoso!")
-    }
+//Autenticando conta
+let usuario = prompt("Digite seu usuário: ");
+let senha = prompt("Digite sua senha: ");
+if (usuario == "admin" && senha == "1234" ){
+    console.log("Login realizado com sucesso!")
 }
 else{
-    console.log("Insira uma idade válida! (<0)")
+    console.log("Falha de autenticação.")
 }
