723217ac3e0d3fbd4d124805ae11643d655b4474
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:48:09 -0300

    Exercício 6 - Bruno Biletsky

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-//Autenticando conta
-let usuario = prompt("Digite seu usuário: ");
-let senha = prompt("Digite sua senha: ");
-if (usuario == "admin" && senha == "1234" ){
-    console.log("Login realizado com sucesso!")
+//Cálculo de aprovação
+ 
+let nota1 = parseFloat(prompt("Digite a primeira nota da média: "));
+let nota2 = parseFloat(prompt("Digite a segunda nota da média: "));
+let nota3 = parseFloat(prompt("Digite a terceira nota da média: "));
+let nota4 = parseFloat(prompt("Digite a quarta nota da média: "));
+let nota5 = parseFloat(prompt("Digite a quinta nota da média: "));
+let nota6 = parseFloat(prompt("Digite a sexta nota da média: "));
+let nota7 = parseFloat(prompt("Digite a sétima nota da média: "));
+let media = parseFloat((nota1+nota2+nota3+nota4+nota5+nota6+nota7)/7)
+if(media>=6){
+    console.log(media,"Aprovado!")
 }
 else{
-    console.log("Falha de autenticação.")
+    console.log(media,"Reprovado.")
 }
