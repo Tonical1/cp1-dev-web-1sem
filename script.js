34e4cd2842da8be983164b6f7825647038b6e656
Enrico <errodrigues2006@outlook.com.br>
2024-03-27 15:52:59 -0300

    Exercício 9 - Victor Freire

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Buscador de termos
-let texto = "Tecnologia é um produto da ciência e da engenharia que envolve um conjunto de instrumentos, métodos e técnicas que visam a resolução de problemas";
-console.log(texto.indexOf("em"))
-console.log(texto.lastIndexOf("ia"))
-console.log(texto.slice(27,34))
-console.log(texto.slice(92,98))
+//string -> float -> dizer variavel
+let variavel = prompt("Digite a string: ");
+variavel.toString()
+let variavelfloat = parseFloat(variavel);
+let verificando = typeof(variavelfloat);
+console.log(verificando)
